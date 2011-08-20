1ebb51009359618e60097845151fa9ef3088d840
Chris Freeman <cwfreeman@gmail.com>
2011-08-20 16:53:55 -0500

    Added project file.

---
+++ language-shootout/clojure/project.clj
@@ -0,0 +1,12 @@
+(defproject language-shootout/clojure "1.0-SNAPSHOT"
+            :description ""
+            :dependencies [
+                           [org.clojure/clojure "1.2.1"]
+                           [org.clojure/clojure-contrib "1.2.0"]
+                           [compojure "0.6.4"]
+                           ]
+            :dev-dependencies [
+                               [midje "1.3-alpha1"]
+                               [lein-midje "[1.0.0,)"]
+                               ]
+)
