eb1c34a9c391a3740b3a8f4302a587f3b0e09628
Chris Freeman <cwfreeman@gmail.com>
2011-08-20 17:23:21 -0500

    Added Ring dependency.

--- language-shootout/clojure/project.clj
+++ language-shootout/clojure/project.clj
@@ -4,9 +4,12 @@
                            [org.clojure/clojure "1.2.1"]
                            [org.clojure/clojure-contrib "1.2.0"]
                            [compojure "0.6.4"]
+                           [ring/ring-core "0.2.0"]
+                           [ring/ring-jetty-adapter "0.2.0"]
                            ]
             :dev-dependencies [
                                [midje "1.3-alpha1"]
                                [lein-midje "[1.0.0,)"]
+                               [ring/ring-devel "[0.2.0]"]
                                ]
 )
