a4c024d315c9c740e4aa673b7072ae906178fb69
Chris Freeman <cwfreeman@gmail.com>
2011-08-20 19:54:03 -0500

    Moved directories around.

--- language-shootout/clojure/src/com/cwfreeman/language-shootout/core.clj
+++
@@ -1,3 +0,0 @@
-(ns "com.cwfreeman.language.shootout")
-
-
---
+++ language-shootout/clojure/test/com/cwfreeman/language-shootout/test/core.clj
@@ -0,0 +1,6 @@
+(ns "com.cwfreeman.language-shootout.test"
+    (:use midje.sweet)
+    (:use com.cwfreeman.language-shootout)
+)
+
+(fact (+ 1 1) 2)
