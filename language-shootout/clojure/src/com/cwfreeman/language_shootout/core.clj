b486d302dc4036e65dbe28762be844d7fbeabf7d
Chris Freeman <cwfreeman@gmail.com>
2011-08-20 22:39:59 -0500

    Removed some extraneous stuff.

--- language-shootout/clojure/src/com/cwfreeman/language_shootout/core.clj
+++ language-shootout/clojure/src/com/cwfreeman/language_shootout/core.clj
@@ -1,10 +1,8 @@
 (ns com.cwfreeman.language-shootout.core
   (:use
-       ; [ring.util.response :only (file-response resource-response)]
         [compojure.core :only (defroutes GET)])
   (:require
             [compojure.route :as route]
-;            [compojure.handler :as handler]
             [ring.adapter.jetty :as ring]
             )
 )
@@ -21,9 +19,6 @@
            (GET "/" [] get-statuses)
            (route/resources "/")
 )
-
-; (def application (handler/site routes))
-
 
 (defn start [port]
     (ring/run-jetty routes {:port (or port 80) :join? false}))
