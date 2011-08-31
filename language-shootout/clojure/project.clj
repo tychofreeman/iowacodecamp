bc84bf35b39dc1b568401145ad24e477807a441d
Chris Freeman <cwfreeman@gmail.com>
2011-08-31 08:27:00 -0500

    Added changes to project.clj.

--- language-shootout/clojure/project.clj
+++ language-shootout/clojure/project.clj
@@ -1,17 +1,20 @@
 (defproject language-shootout/clojure "1.0-SNAPSHOT"
             :description ""
             :dependencies [
-                           [org.clojure/clojure "1.2.1"]
-                           [org.clojure/clojure-contrib "1.2.0"]
-                           [compojure "0.6.5"]
-                           [ring/ring-core "0.3.11"]
-                           [ring/ring-jetty-adapter "0.3.11"]
-                           [sandbar/sandbar "0.4.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.6.5"]
+                 [ring/ring-core "0.3.11"]
+                 [ring/ring-jetty-adapter "0.3.11"]
+                 [sandbar/sandbar "0.4.0-SNAPSHOT"]
+                 [postgresql/postgresql "8.4-702.jdbc4"]
+                 [org.clojure/java.jdbc "0.0.3-SNAPSHOT"]
                            ]
             :dev-dependencies [
                                [midje "1.3-alpha1"]
                                [lein-midje "[1.0.2,)"]
 ;                               [ring/ring-devel "[0.2.0]"]
                                ]
+            :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
             :main main
 )
