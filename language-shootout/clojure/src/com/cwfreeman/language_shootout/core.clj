daa06cefb3f43f3f84310888efbdb3a3e64a928b
Chris Freeman <cwfreeman@gmail.com>
2011-09-03 11:59:24 -0500

    Quick commit...

--- language-shootout/clojure/src/com/cwfreeman/language_shootout/core.clj
+++ language-shootout/clojure/src/com/cwfreeman/language_shootout/core.clj
@@ -1,29 +1,73 @@
 (ns com.cwfreeman.language-shootout.core
   (:use
         [com.cwfreeman.language-shootout.statuses]
-        [compojure.core :only (defroutes GET)])
+        [compojure.core :only (defroutes GET PUT POST)]
+)
   (:require
-            [compojure.route :as route]
-            [ring.adapter.jetty :as ring]
-            [sandbar.core :as sandbar]
-            [sandbar.stateful-session :as ss]
-            [clojure.contrib.json :as json]
-            )
+        [compojure.route :as route]
+        [ring.adapter.jetty :as ring]
+        [ring.middleware.params :as params]
+        [clojure.contrib.json :as json]
+        [hiccup.core :as hiccup]
+        [sandbar.auth :as auth]
+        [sandbar.core :as sandbar]
+        [sandbar.form-authentication :as fauth]
+        [sandbar.stateful-session :as ss]
+        [sandbar.validation]
+        )
 )
 
+(def security-policy
+  [
+    #"/login" #{:any}
+    #".*" #{:any}
+    ])
 
-(def four-oh-four "<html><head><title>404</title><head><body>You can't go there</body></html>")
+; this can be expanded to do whatever you want. :name and :roles are required, though.
+(defn auth-fn [request]
+  (println "auth-fn...")
+  {:name "Bob" :roles [:any]})
 
-(defroutes routes 
-   (GET "/statuses" [] (json/json-str (get-statuses)))
-   (GET "/replies/status=:id" [id] (json/json-str (get-replies (Integer/parseInt id))))
-   (GET "/status/:id" [id] (json/json-str (get-status (Integer/parseInt id))))
-   (route/resources "/")
-   (route/not-found four-oh-four)
+
+(defn four-oh-four [req]
+  (str "You can't go there - " req " duh"))
+
+(def login-screen
+  (str  "<html>"
+          "<head><title>Login</title>"
+          "<body>"
+            "<form method='post'>"
+              "<label>Username</label><input type='text' name='username'>"
+              "<br/>"
+              "<label>Password</label><input type='password' name='password'>"
+              "<input type='submit' name='login'></input>"
+            "</form>"
+          "</body>"
+        "</head>"
+    ))
+
+(defn am-i-logged-in [p]
+  (println p))
+
+(defroutes routes
+  (GET "/login" [] login-screen)
+  (POST "/login" {params :params} (am-i-logged-in :params))
+  (GET "/statuses" [] (json/json-str (get-statuses)))
+  (GET "/replies/status=:id" [id] (json/json-str (get-replies (Integer/parseInt id))))
+  (POST "/status" {params :params} (json/json-str (add-status params)))
+  (GET "/status/:id" [id] (json/json-str (get-status (Integer/parseInt id))))
+  (route/files "/")
+  (GET "*" req (four-oh-four req))
+  (POST "*" req (four-oh-four req))
+  (route/not-found (four-oh-four "not found"))
 )
+
 
 (def app (-> routes
-           ss/wrap-stateful-session))
+           (auth/with-security security-policy auth-fn) ; this MUST precede wrap-stateful-session
+           ss/wrap-stateful-session
+           params/wrap-params
+           ))
 
 (defn start [port]
     (ring/run-jetty app {:port (or port 80) :join? false})
--- language-shootout/clojure/src/com/cwfreeman/language_shootout/statuses.clj
+++ language-shootout/clojure/src/com/cwfreeman/language_shootout/statuses.clj
@@ -1,20 +1,37 @@
 (ns com.cwfreeman.language-shootout.statuses
   )
 
+
+(def curr-id (atom 0))
+(defn next-id []
+  (swap! curr-id + 1))
+
+(defn status [user msg]
+  {:id (next-id) :text msg :name user :date :now})
+
+
 (def statuses
-  [{:id 1 :text "I am awake" :name "Bob" :date :now}
-   {:id 2 :text "I am asleep" :name "Bob" :date :earlier}])
+  (atom [
+          (status "Bob" "I am awake")
+          (status "Bob" "I am asleep")
+          ]))
 
 (def replies
   [{:text "Me too!!" :name "Sam" :date :now :status 1}
    {:text "Me too!!" :name "Sam" :date :now :status 2}])
 
 (defn get-statuses [req]
-  statuses)
+  @statuses)
 
 (defn get-replies [id]
       (filter #(= (:status %1) id) replies))
 
 (defn get-status [id]
-    {:status (filter #(= (:id %1) id) statuses)
+    {:status (filter #(= (:id %1) id) @statuses)
      :replies [(get-replies id)]})
+
+(defn add-status [p]
+  (let [s (status "Bob" (get p "statusText"))]
+  (reset! statuses (conj @statuses s))
+  (println @statuses)
+  s))
