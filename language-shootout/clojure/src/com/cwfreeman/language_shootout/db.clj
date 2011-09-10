b7bf4a8d7e8bafbee0a66b90ed184cbfabe25506
Chris Freeman <cwfreeman@gmail.com>
2011-09-10 10:55:48 -0500

    ...

--- language-shootout/clojure/src/com/cwfreeman/language_shootout/db.clj
+++ language-shootout/clojure/src/com/cwfreeman/language_shootout/db.clj
@@ -10,5 +10,23 @@
     :password    ""
    })
 
-; And this is how to connect to it.
-; (sql/with-connection db-conn (sql/with-query-results res ["select * from db"])))
+  (defn params-to-vec [query params]
+          (vec (flatten (filter #(not (nil? %)) [[query] params]))))
+
+  (defn init-db []
+    (sql/with-connection db-conn 
+      (sql/create-table :users [:id "IDENTITY"] [:name "varchar"] [:password "varchar"])
+      (sql/create-table :roles [:id "IDENTITY"] [:name "varchar"])
+      (sql/create-table :userroles [:userid "integer references users(id)"] [:roleid "integer references roles(id)"])
+      (sql/insert-rows :roles [1 :admin] [2 :any])
+      (sql/insert-rows :users [1 :bob "any"] [2 :amy "any"])
+      (sql/insert-rows :userroles [1 1] [1 2] [2 1] [2 2])
+    ))
+
+  (defn find-user [username]
+    (sql/with-connection db-conn
+      (sql/with-query-results res (params-to-vec "SELECT roles.name as role from users inner join userrules as ur on ur.userid = users.id inner join roles on ur.roleid = roles.id where users.name = ?" username)
+        #{ res })))
+
+
+;; --- Use sql/do-commands or do-prepared or something. with-connection will create a new connection
--- language-shootout/clojure/src/main.clj
+++ language-shootout/clojure/src/main.clj
@@ -1,5 +1,7 @@
 (ns main
-  (:use com.cwfreeman.language-shootout.core))
+  (:use com.cwfreeman.language-shootout.core
+        com.cwfreeman.language-shootout.db))
 
 (defn -main []
+    (com.cwfreeman.language-shootout.db/init-db)
     (start 8999))
